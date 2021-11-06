8e359b53f7cb094d188501841c2769ec5e306beb
Elie BAMUNOBA <Etb28stic@>
2021-11-07 02:50:48 +0300

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
               brightness: _brightness,
             ),
             onGenerateRoute: route.controller,
-            initialRoute: route.homeTabs,
+            initialRoute: route.startupViewRoute,
           );
         });
   }
